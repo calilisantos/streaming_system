1e0ead93e4d5f9b0283a57ccb74756c019a1418a
calilisantos <calilisantos@gmail.com>
2025-03-17 15:29:07 -0300

    feat(events_generator): code notes

--- events_generator/src/main.py
+++ events_generator/src/main.py
@@ -1,3 +1,9 @@
-# dicts with user_id and product_id
-# a method to every action (login, click, purchase) with user_id and product_id variables
-# and a method to generate a random event with a random user_id and product_id
+"""
+Classes with user_id and product_id
+Class getting envs to write on the topic
+Class Actions with a method to every action (login, click, purchase) with user_id and product_id variables
+Class Write with: 
+  i. a method to generate a random event with a random user_id and product_id; 
+  ii. a method to write the event on the topic
+With a frequency of 1 event per 10 seconds
+"""
