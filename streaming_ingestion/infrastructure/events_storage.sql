7296a59847e54f30d680af3c16b14f71be66fc4f
calilisantos <calilisantos@gmail.com>
2025-03-20 00:30:00 -0300

    style(events_storage): fine adjusts

--- streaming_ingestion/infrastructure/events_storage.sql
+++ streaming_ingestion/infrastructure/events_storage.sql
@@ -1,49 +1,16 @@
--- CREATE DATABASE IF NOT EXISTS events_storage;
-
--- USE events_storage;
-
--- -- Contagem total de eventos por tipo (exemplo: clique, login, compra)
--- CREATE TABLE IF NOT EXISTS event_counts(
--- 	event_type VARCHAR(100) NOT NULL,
---     occurrences BIGINT DEFAULT 0,
---     PRIMARY KEY (event_type)
--- );
-
--- -- Contagem de usuários únicos que realizaram cada tipo de evento
--- CREATE TABLE IF NOT EXISTS user_event_counts(
---   event_type VARCHAR(100) NOT NULL,
---     `user_id` BIGINT NOT NULL,
---     occurrences BIGINT DEFAULT 0,
---     PRIMARY KEY (event_type, `user_id`)
--- );
-
--- -- Tempo médio entre eventos por usuário
--- CREATE TABLE IF NOT EXISTS user_avg_waiting_time(
---   `user_id` BIGINT NOT NULL,
---     avg_waiting_time DECIMAL(10,2) NOT NULL,
---     PRIMARY KEY (`user_id`)
--- );
-
--- Conectar ao banco de dados
-
--- Criação do banco de dados
--- CREATE DATABASE IF NOT EXISTS events_storage;
 \c events_storage;
 
--- Contagem total de eventos por tipo (exemplo: clique, login, compra)
 CREATE TABLE IF NOT EXISTS event_counts (
     event_type VARCHAR(100) NOT NULL,
     occurrences BIGINT DEFAULT 0
 );
 
--- Contagem de usuários únicos que realizaram cada tipo de evento
 CREATE TABLE IF NOT EXISTS user_event_counts (
     event_type VARCHAR(100) NOT NULL,
     user_id BIGINT NOT NULL,
     occurrences BIGINT DEFAULT 0
 );
 
--- Tempo médio entre eventos por usuário
 CREATE TABLE IF NOT EXISTS user_avg_waiting_time (
     user_id BIGINT NOT NULL,
     avg_waiting_time DECIMAL(10, 2) NOT NULL
