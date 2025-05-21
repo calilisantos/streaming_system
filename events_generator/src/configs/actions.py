33cf806c978a9b6c738d1c3cfd9b84a57a80a3e9
calilisantos <calilisantos@gmail.com>
2025-05-21 12:47:04 -0300

    refac entities

--- events_generator/src/configs/actions.py
+++
@@ -1,3 +0,0 @@
-class Actions:
-    ACTIONS_NAMES = ["login", "click", "purchase"]
-    ACTIONS_RANGE = len(ACTIONS_NAMES)
--- events_generator/src/configs/users.py
+++
@@ -1,8 +0,0 @@
-class Users:
-    USERS_IDS = [
-        234,
-        5678,
-        208,
-        619
-    ]
-    USERS_RANGE = len(USERS_IDS)
