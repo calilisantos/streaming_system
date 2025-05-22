d2dce23c4d1686e31be9f764db621636b37630ab
calilisantos <calilisantos@gmail.com>
2025-05-22 12:59:40 -0300

    feat(events_generator): add logs confs

--- events_generator/src/main.py
+++ events_generator/src/main.py
@@ -1,6 +1,8 @@
+from configs.logs import Logs
 from application.events_generator import EventsGenerator
 from infrastructure.configs.app_config import AppConfig
 
 if __name__ == "__main__":
+    Logs.configure()
     config = AppConfig()
     EventsGenerator(config).start()
