27eeacfebd74105ec4a71784bf4f88e5997dbd97
calilisantos <calilisantos@gmail.com>
2025-05-22 02:18:24 -0300

    build(docker-compose): kafka services tags

--- events_generator/src/configs/topic.py
+++ events_generator/src/configs/topic.py
@@ -1,12 +1,5 @@
-from dotenv import load_dotenv
-from os import getenv
-
-
-load_dotenv()
-
-
 class Topic:
     BOOTSTRAP_SERVERS = "kafka-server:9092"
     DEFAULT_ENCODE = "utf-8"
-    TOPIC_NAME = getenv("TOPIC_NAME")
+    TOPIC_NAME = "app-events"
     WRITE_INTERVAL = 5
